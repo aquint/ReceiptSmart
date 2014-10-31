cba3d8777b6686bb77d37a5359d1726c1cbaf10e
aquint <aquinton_22@hotmail.com>
2014-10-31 17:17:48 -0400

    configure

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,7 @@ class UserMailer < ActionMailer::Base
   #
   #   en.user_mailer.account_activation.subject
   #
-  def account_activation
+  def account_activation(user)
     @user = user
     mail to: user.email, subject: "Account Activation"
   end
